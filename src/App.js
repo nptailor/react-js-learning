eaa83100fccd54dc4b7673cadc037ef9a25b7f24
Nidhi <nidhi@auberginesolutions.com>
2020-04-14 18:50:13 +0530

    styling components and working with lists and conditions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,114 @@
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
-import { render } from '@testing-library/react';
-class App extends Component{
-  state={
-    person:[
-      {name:"Venessa",age:40},
-      {name:"Dan", age:38},
-      {name:"Olivia", age:30}
-    ]
-  }
-  render(){
-    return (
-      <div className="App">
-       <h1>Hi React App</h1>
-       <p>Hi I am a paragraph in react App</p>
-       <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-       <Person name={this.state.person[1].name} age={this.state.person[1].age}>I am a Doctor !</Person>
-       <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-      </div>
-    );
-    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,"Hi i am from create element"))
-  }
+
+
+
+class App extends Component {
+
+  state = ({
+      person:[
+        {id:'abc1',name:"Venessa",age:40},
+        {id:'abc2',name:"Dan", age:38},
+        {id:'abc3',name:"Olivia", age:30}
+      ],
+      showPersons:false
+    })
+
+  //function that changes name of persons on click
+  switchNameHandler = (newName) =>{
+    this.setState({
+      person:[
+        {name:newName,age:40},
+        {name:"Nate", age:38},
+        {name:"Olivia", age:21}
+      ]
+    })
+    }
+
+
+    //function that changes name of person based on input box value. called on change 
+    changeNameHandler = (event,id) =>{
+      const personIndex = this.state.person.findIndex(p=>{
+        return p.id === id;
+      })
+
+      const prsn = {
+        ...this.state.person[personIndex]
+      }
+
+      prsn.name = event.target.value;
+
+      const persons = [...this.state.person]
+      persons[personIndex] = prsn;
+
+      this.setState({person: persons})
+    }
+
+
+    //function that shows/hides data of persons on button click
+    togglePersonsHandler =() =>{
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
+    }
+
+
+    //function that deletes a person's data when clicking on its box
+    deletePersonHandler = (personIndex) =>{
+      const persons =[ ...this.state.person];
+      persons.splice(personIndex,1);
+      this.setState({person: persons});
+    }
+
+
+    //render function , that sends data to the html
+    render(){
+        //in-line styling for button
+        const style={
+          backgroundColor:'lightblue',
+          font: 'inherit',
+          border: '1px solid blue',
+          padding:'10px',
+          cursor: 'pointer'
+        };
+        
+        //variable used to display no persons data on page load and on button clicking
+        let persons = null;
+        
+        //if condition used to display person's data based on its boolean value
+        if(this.state.showPersons === true){
+          persons = (
+            <div>
+              {
+                this.state.person.map((prsn,index) => {
+                return <Person 
+                name={prsn.name} 
+                age={prsn.age}
+                click={this.deletePersonHandler.bind(this,index)}
+                key={prsn.id}
+                changed={(event) => this.changeNameHandler(event,prsn.id)}/>
+              })
+              }
+            </div>
+          );
+        }
+
+
+        return (
+          <div className="App">
+            <h1>Hi React App</h1>
+            <p>Hi I am a paragraph in react App</p>
+            <button 
+              style={style}
+              onClick={this.togglePersonsHandler}>Switch Name
+            </button>
+
+            {/*  Data shown based on the persons value */}
+            {persons}
+          </div>
+        );
+        // return React.createElement('div',{className: 'App'},React.createElement('h1',null,"Hi i am from create element"))
+      }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Person.css';
 
 const person =(props)=>{
 return (
-<div>
-<p>I am {props.name}. My age is {props.age}</p>
-<label>{props.children}</label>
+<div className="Person">
+    <p onClick={props.click}>I am {props.name}. My age is {props.age}</p>
+    <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name}/>
 </div>
 );
 }
